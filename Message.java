6e559a00f8177d30525f3ef3c35de5602c7a264f
chetanpatidar1006 <40052142+chetanpatidar1006@users.noreply.github.com>
2024-07-07 12:46:47 +0530

    Create Message.java
    Create New File

---
+++ Message.java
@@ -0,0 +1,11 @@
+class Message {
+     private String content;
+     // Constructs a message with the given content.
+    public Message(String content) {
+        this.content = content;
+    }
+    // Retrieves the content of the message.
+    public String getContent() {
+        return content;
+    }
+}
